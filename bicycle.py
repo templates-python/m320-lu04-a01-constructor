285c33aaacf8db052936b55bfc43546db11d8401
Marcel Suter <marcel.suter@bzz.ch>
2024-08-20 08:58:08 +0200

    added solution

--- bicycle.py
+++ bicycle.py
@@ -1,21 +1,89 @@
 class Bicycle:
-    '''
-    Definiert ein Fahrrad mit einigen wenigen Attributen und Methoden.
-    '''
+    """
+    A bicycle with some attributes and methods
 
-# hier Konstruktor anschreiben
-# Konstruktor mit Testmethode test_initialization-Methode überprüfen
+    Attributes
+    ----------
+    _type: str
+    _size: int
+    _color: str
+    """
 
-# hier getter und setter der Attribute anschreiben
-# Jedes paar (get/set) mit entsprechender Testmethode überprüfen
-# - test_type_set_get
-# - test_size_get_set
-# - test_color_get_set
+    def __init__(self, type='mountainbike', size=48):
+        """
+        Konstruktor initialisiert das Objekt mit 3 Parameterwerten.
+        """
+        self._type = type
+        self._size = size
+        self._color = 'gray'
+
+    @property
+    def type(self):
+        """
+        Liefert den Typ des Fahrrads
+        """
+        return self._type
+
+    @property
+    def size(self):
+        """
+        Liefert die Grösse des Fahrrads
+        """
+        return self._size
+
+    @property
+    def color(self):
+        """
+        Liefert die Farbe des Fahrrads
+        """
+        return self._color
+
+    @type.setter
+    def type(self, type):
+        """
+        Legt den Typ des Fahrrads fest
+        """
+        self._type = type
+
+    @size.setter
+    def size(self, size):
+        """
+        Legt die Grösse des Fahrrads fest
+        """
+        self._size = size
+
+    @color.setter
+    def color(self, color):
+        """
+        Legt die Farbe des Fahrrads fest
+        """
+        self._color = color
+
+    def print(self):
+        """
+        prints a human-readable representation of the object
+        :return:
+        """
+        print(
+            f'Fahrrad:\n'
+            f'\tArt:            {self._type}\n'
+            f'\tRahmengrösse:   {self._size}\n'
+            f'\tFarbe:          {self._color}'
+        )
+
 
 if __name__ == "__main__":
-    pass
     # Aufgabe 1
-    # Objekt erzeugen und über print-Methode des Objekts(!) ausgeben.
+    bicycle = Bicycle()
+    bicycle.print()
+    #
+    bicycle = Bicycle(type='racebike')
+    bicycle.print()
+    #
+    bicycle = Bicycle(type='citybike', size='42')
+    bicycle.print()
     #
     # Aufgabe 2
-    # Farbe bei 3. Objekt auf 'red' setzen
+    #
+    bicycle.color = 'red'
+    bicycle.print()
